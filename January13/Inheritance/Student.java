10cdcf5ac3cc992574ceac396ebedd462b088df6
Mohamed Alie Pussah, II <talk2alie@outlook.com>
2020-01-13 17:35:26 -0500

    Add Student class

---
+++ January13/Inheritance/Student.java
@@ -0,0 +1,23 @@
+import java.time.LocalDate;
+
+/**
+ * Student
+ */
+public class Student extends Person {
+
+    private final int id;
+
+    public Student() {
+        super();
+        id = getCount();
+    }
+
+    public Student(String firstName, String lastName, LocalDate birthDate) {
+        super(firstName, lastName, birthDate);
+        id = getCount();
+    }
+
+    public int getId() {
+        return id;
+    }
+}
